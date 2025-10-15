d9d691200a5fa033bcc3e7db796fc72fa2c3735e
Camilla Coronado <camilla.coronado@gmail.com>
2025-10-14 21:32:43 -0700

    env variable changes

--- src/routes/api/session/+server.ts
+++ src/routes/api/session/+server.ts
@@ -6,9 +6,12 @@ import type { RequestHandler } from './$types';
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    projectId: 'sfos-ba7a0'
-  });
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+  })
+});
 }
 
 export const POST: RequestHandler = async ({ request, cookies }) => {
